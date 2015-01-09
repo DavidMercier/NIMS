f4398fad452a8aba9e07e2b2fe9f85c3cb221b58
unknown <d.mercier@mapc103.mpie.de>
2015-01-09 18:03:05 +0100

    refactored code in the plot functions

--- Matlab_Code/plot_exp_vs_mod.m
+++ Matlab_Code/plot_exp_vs_mod.m
@@ -6,6 +6,7 @@ gui = guidata(gcf);
 %% Initialization
 set(gui.handles.MainWindows, 'CurrentAxes', gui.handles.AxisPlot_GUI);
 cla;
+legend_str = '';
 
 gui.variables.log_plot_value = get(gui.handles.cb_log_plot_GUI, 'Value');
 
@@ -58,25 +59,31 @@ if gui.variables.log_plot_value == 0
     if gui.variables.val2 == 1
         
         if gui.variables.y_axis ~= 1
-            gui.handles.plot_data = plot(gui.axis.x2plot, gui.axis.y2plot, 'rx', gui.axis.xline, gui.axis.yline,  '--ko', 'LineWidth', 2);
-            legend('Initial data', 'Location', 'NorthWest');
-            title(gui.axis.title_str, 'FontSize', 18);
+            gui.handles.plot_data = plot(...
+                gui.axis.x2plot, gui.axis.y2plot, 'rx', ...
+                gui.axis.xline, gui.axis.yline,  '--ko');
+            legend_str = 'Initial data';
         else
-            gui.handles.plot_data = plot(gui.axis.x2plot, gui.axis.y2plot, 'rx', gui.axis.x2plot, gui.axis.y2plot_2, 'b-', gui.axis.xline, gui.axis.yline,  '--ko', 'LineWidth', 2);
-            legend('Initial data','Polynomial Model', 'Location', 'NorthWest');
-            title(gui.axis.title_str, 'FontSize', 18);
+            gui.handles.plot_data = plot(...
+                gui.axis.x2plot, gui.axis.y2plot, 'rx', ...
+                gui.axis.x2plot, gui.axis.y2plot_2, 'b-', ...
+                gui.axis.xline, gui.axis.yline,  '--ko');
+            legend_str = {'Initial data', 'Polynomial Model'};
         end
         
     elseif gui.variables.val2 ~= 1
         
-        if gui.variables.y_axis == 3 || gui.variables.y_axis == 4
-            gui.handles.plot_data = plot(gui.axis.x2plot, gui.axis.y2plot, 'rx', gui.axis.x2plot, gui.axis.y2plot_2, 'b-', gui.axis.xline, gui.axis.yline, '--ko', 'LineWidth', 2);
-            legend('Initial data', gui.axis.legend2, 'Location', 'NorthWest');
+        if gui.variables.y_axis == 4 || gui.variables.y_axis == 5
+            gui.handles.plot_data = plot(...
+                gui.axis.x2plot, gui.axis.y2plot, 'rx', ...
+                gui.axis.x2plot, gui.axis.y2plot_2, 'b-', ...
+                gui.axis.xline, gui.axis.yline, '--ko');
+            legend_str = {'Initial data', gui.axis.legend2};
         else
-            gui.handles.plot_data = plot(gui.axis.x2plot, gui.axis.y2plot, 'rx', gui.axis.xline, gui.axis.yline, '--ko', 'LineWidth', 2);
-            legend('Initial data', 'Location', 'NorthWest');
-            title(gui.axis.title_str, 'FontSize', 18);
-            
+            gui.handles.plot_data = plot(...
+                gui.axis.x2plot, gui.axis.y2plot, 'rx', ...
+                gui.axis.xline, gui.axis.yline, '--ko');
+            legend_str = 'Initial data';
         end
     end
     
@@ -84,24 +91,31 @@ elseif gui.variables.log_plot_value == 1
     
     if gui.variables.val2 == 1
         if gui.variables.y_axis ~= 1
-            gui.handles.plot_data = loglog(gui.axis.x2plot, gui.axis.y2plot, 'rx', gui.axis.xline, gui.axis.yline, '--ko', 'LineWidth', 2);
-            legend('Initial data', 'Location', 'NorthWest');
-            title(gui.axis.title_str, 'FontSize', 18);
+            gui.handles.plot_data = loglog(...
+                gui.axis.x2plot, gui.axis.y2plot, 'rx', ...
+                gui.axis.xline, gui.axis.yline, '--ko');
+            legend_str = 'Initial data';
         else
-            gui.handles.plot_data = loglog(gui.axis.x2plot, gui.axis.y2plot, 'rx', gui.axis.x2plot, gui.results.P_fit, 'b-', gui.axis.xline, gui.axis.yline, '--ko', 'LineWidth', 2);
-            legend('Initial data', 'Polynomial Model', 'Location', 'NorthWest');
-            title(gui.axis.title_str, 'FontSize', 18);
+            gui.handles.plot_data = loglog(...
+                gui.axis.x2plot, gui.axis.y2plot, 'rx', ...
+                gui.axis.x2plot, gui.results.P_fit, 'b-', ...
+                gui.axis.xline, gui.axis.yline, '--ko');
+            legend_str = {'Initial data', 'Polynomial Model'};
         end
         
     elseif gui.variables.val2 ~= 1
         
-        if gui.variables.y_axis == 3 || gui.variables.y_axis == 4
-            gui.handles.plot_data = loglog(gui.axis.x2plot, gui.axis.y2plot, 'rx', gui.axis.x2plot, gui.axis.y2plot_2, 'b-', gui.axis.xline, gui.axis.yline, '--ko', 'LineWidth', 2);
-            legend('Initial data', gui.axis.legend2, 'Location', 'NorthWest');
+        if gui.variables.y_axis == 4 || gui.variables.y_axis == 5
+            gui.handles.plot_data = loglog(...
+                gui.axis.x2plot, gui.axis.y2plot, 'rx', ...
+                gui.axis.x2plot, gui.axis.y2plot_2, 'b-', ...
+                gui.axis.xline, gui.axis.yline, '--ko');
+            legend_str = {'Initial data', gui.axis.legend2};
         else
-            gui.handles.plot_data = loglog(gui.axis.x2plot, gui.axis.y2plot, 'rx', gui.axis.xline, gui.axis.yline, '--ko', 'LineWidth', 2);
-            legend('Initial data', 'Location', 'NorthWest');
-            title(gui.axis.title_str, 'FontSize', 18);
+            gui.handles.plot_data = loglog(...
+                gui.axis.x2plot, gui.axis.y2plot, 'rx', ...
+                gui.axis.xline, gui.axis.yline, '--ko');
+            legend_str = 'Initial data';
         end
     end
 end
@@ -111,59 +125,59 @@ if get(gui.handles.value_numthinfilm_GUI, 'Value') ~= 1
     text(gui.axis.xline(1), gui.axis.ymax-0.05*gui.axis.ymax, ' \leftarrow Thin Film 0', 'FontSize', 12);
 end
 
-if gui.variables.num_thinfilm == 1 && ((gui.variables.y_axis == 3) || (gui.variables.y_axis == 4))
+if gui.variables.num_thinfilm == 1 && (gui.variables.y_axis == 4 || gui.variables.y_axis == 5)
     set(gui.handles.value_youngsub_GUI, 'String', round(mean(gui.results.Esample)));
-    title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(mean(gui.results.Esample))), 'GPa'], 'FontSize', 18);
+    gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(mean(gui.results.Esample))), 'GPa'];
     
 elseif gui.variables.num_thinfilm == 2
     
-    if gui.variables.y_axis == 3 && gui.variables.val2 < 6
+    if gui.variables.y_axis == 4 && gui.variables.val2 < 6
         set(gui.handles.value_youngfilm0_GUI, 'String', round(gui.results.Ef_sol_fit));
-        title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit(1))), 'GPa'], 'FontSize', 18);
-        
-    elseif gui.variables.y_axis == 3 && gui.variables.val2 == 6
-        set(gui.handles.value_youngfilm0_GUI, 'String', round(gui.results.Ef_sol_fit(1)));
-        title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit(1))), 'GPa / n(Perriot et al.) = ', num2str(gui.results.Ef_sol_fit(2))], 'FontSize', 18);
-    
-    elseif gui.variables.y_axis == 3 && gui.variables.val2 == 7
-        set(gui.handles.value_youngfilm0_GUI, 'String', round(gui.results.Ef_sol_fit(1)));
-        title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit(1))), 'GPa / Es = ', num2str(round(gui.results.Ef_sol_fit(2))), 'GPa'], 'FontSize', 18);
-        
-    elseif gui.variables.y_axis == 4 && gui.variables.val2 < 6
-        set(gui.handles.value_youngfilm0_GUI, 'String', round(gui.results.Ef_sol));
-        title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol)), 'GPa'], 'FontSize', 18);
+        gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit(1))), 'GPa'];
         
     elseif gui.variables.y_axis == 4 && gui.variables.val2 == 6
-        set(gui.handles.value_youngfilm0_GUI, 'String', round(gui.results.Ef_sol));
-        title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol)), 'GPa / n(Perriot et al.) = ', num2str(gui.results.Ef_sol_fit(2))], 'FontSize', 18);
-    
+        set(gui.handles.value_youngfilm0_GUI, 'String', round(gui.results.Ef_sol_fit(1)));
+        gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit(1))), 'GPa / n(Perriot et al.) = ', num2str(gui.results.Ef_sol_fit(2))];
+        
     elseif gui.variables.y_axis == 4 && gui.variables.val2 == 7
         set(gui.handles.value_youngfilm0_GUI, 'String', round(gui.results.Ef_sol_fit(1)));
-        title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit(1))), 'GPa / Es = ', num2str(round(gui.results.Ef_sol_fit(2))), 'GPa'], 'FontSize', 18);
+        gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit(1))), 'GPa / Es = ', num2str(round(gui.results.Ef_sol_fit(2))), 'GPa'];
+        
+    elseif gui.variables.y_axis == 5 && gui.variables.val2 < 6
+        set(gui.handles.value_youngfilm0_GUI, 'String', round(gui.results.Ef_sol));
+        gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol)), 'GPa'];
+        
+    elseif gui.variables.y_axis == 5 && gui.variables.val2 == 6
+        set(gui.handles.value_youngfilm0_GUI, 'String', round(gui.results.Ef_sol));
+        gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol)), 'GPa / n(Perriot et al.) = ', num2str(gui.results.Ef_sol_fit(2))];
+        
+    elseif gui.variables.y_axis == 5 && gui.variables.val2 == 7
+        set(gui.handles.value_youngfilm0_GUI, 'String', round(gui.results.Ef_sol_fit(1)));
+        gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit(1))), 'GPa / Es = ', num2str(round(gui.results.Ef_sol_fit(2))), 'GPa'];
         
     end
     
 elseif gui.variables.num_thinfilm == 3
     
-    if gui.variables.y_axis == 3
+    if gui.variables.y_axis == 4
         set(gui.handles.value_youngfilm1_GUI, 'String', round(gui.results.Ef_sol_fit));
-        title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit)), 'GPa'], 'FontSize', 18);
+        gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit)), 'GPa'];
         
-    elseif gui.variables.y_axis == 4
+    elseif gui.variables.y_axis == 5
         set(gui.handles.value_youngfilm1_GUI, 'String', round(gui.results.Ef_sol));
-        title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol)), 'GPa'], 'FontSize', 18);
+        gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol)), 'GPa'];
         
     end
     
 elseif gui.variables.num_thinfilm == 4
     
-    if gui.variables.y_axis == 3
+    if gui.variables.y_axis == 4
         set(gui.handles.value_youngfilm2_GUI, 'String', round(gui.results.Ef_sol_fit));
-        title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit)), 'GPa'], 'FontSize', 18);
+        gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol_fit)), 'GPa'];
         
-    elseif gui.variables.y_axis == 4
+    elseif gui.variables.y_axis == 5
         set(gui.handles.value_youngfilm2_GUI, 'String', round(gui.results.Ef_sol));
-        title(['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol)), 'GPa'], 'FontSize', 18);
+        gui.axis.title_str = ['Mean Young''s modulus of the film (when h < t) = ', num2str(round(gui.results.Ef_sol)), 'GPa'];
         
     end
     
@@ -176,6 +190,11 @@ xlim([gui.axis.xmin gui.axis.xmax]);
 ylim([0 gui.axis.ymax]);
 set(gui.handles.AxisPlot_GUI, 'FontSize', 14);
 set(gui.handles.plot_data, 'MarkerSize', 10);
+title(gui.axis.title_str, 'FontSize', 18);
+legend(legend_str, 'Location', 'NorthWest');
+for ii = 1:length(gui.handles.plot_data)
+    gui.handles.plot_data(ii).LineWidth = 2;
+end
 
 if get(gui.handles.cb_grid_plot_GUI, 'Value') == 1
     grid on;
--- Matlab_Code/plot_exp_vs_mod_setvariables.m
+++ Matlab_Code/plot_exp_vs_mod_setvariables.m
@@ -9,28 +9,26 @@ if gui.flag.flag_data == 0
     %% Initialization
 else
     set(gui.handles.MainWindows, 'CurrentAxes', gui.handles.AxisPlot_GUI);
+    gui.axis.title_str = '';
+    gui.axis.title_str = strcat('Max displacement = ', ...
+        num2str(round(max(gui.data.h))), 'nm');
     
     if gui.variables.num_thinfilm == 1 % Only a bulk material
-        
         for ii = 1:1:length(gui.data.h)
             gui.results.Ef_red(ii) = 0;
             gui.results.Ef_red     = gui.results.Ef_red.';
             gui.results.Em_red(ii) = 0;
             gui.results.Em_red     = gui.results.Em_red.';
         end
-        
         gui.data.t = max(gui.data.h)+1;
         gui.results.Ef_sol_fit = 0;
-        
     end
     
-    if gui.variables.y_axis == 4
-        
+    if gui.variables.y_axis == 5
         for ii = 1:1:length(gui.data.h)
             if gui.data.h(ii) < gui.data.t && ...
                     gui.variables.num_thinfilm == 1
                 gui.results.Ef_mean(ii) = gui.results.Ef_red(ii);
-                
             elseif gui.data.h(ii) < gui.data.t && ...
                     gui.variables.num_thinfilm ~= 1
                 gui.results.Ef_mean(ii) = gui.results.Ef(ii);
@@ -46,25 +44,16 @@ else
         gui.axis.xlabelstr = 'Displacement (h) (nm)';
         gui.axis.xmax = round(max(gui.data.h));
         gui.axis.xmin = 0;
-        gui.axis.title_str = strcat('Max displacement = ', ...
-            num2str(round(max(gui.data.h))), 'nm');
-        
     elseif gui.variables.x_axis == 2
         gui.axis.x2plot = gui.results.ac./gui.data.t;
-        gui.axis.xlabelstr = 'Contact Radius / Film Thickness(t)';
+        gui.axis.xlabelstr = 'Contact Radius / Film Thickness (t)';
         gui.axis.xmax = round(max(gui.results.ac./gui.data.t));
         gui.axis.xmin = round(min(gui.results.ac./gui.data.t));
-        gui.axis.title_str = strcat('Max displacement = ', ...
-            num2str(round(max(gui.data.h))), 'nm');
-        
     elseif gui.variables.x_axis == 3
         gui.axis.x2plot = gui.data.h/gui.data.t;
         gui.axis.xlabelstr = 'Displ. (h) / Film Thickness (t)';
         gui.axis.xmax = round(max(gui.data.h/gui.data.t));
         gui.axis.xmin = 0;
-        gui.axis.title_str = strcat('Max displacement = ', ...
-            num2str(round(max(gui.data.h))), 'nm');
-        
     end
     
     if gui.variables.y_axis == 1
@@ -77,40 +66,38 @@ else
             num2str(gui.results.W), 'µJ', ' / ', ...
             ' Exposant of the power law fit  = ', ...
             num2str(gui.results.exp_fit));
-        
     elseif gui.variables.y_axis == 2
         gui.axis.y2plot = gui.data.S;
         gui.axis.delta_y2plot = gui.data.delta_S;
         gui.axis.ylabelstr = 'Stiffness (mN/nm)';
         gui.axis.ymax = round(max(gui.data.S.*1000)./10)./100;
-        gui.axis.title_str = '';
-        
     elseif gui.variables.y_axis == 3
+        gui.axis.y2plot = gui.data.P ./ (gui.data.S.^2);
+        gui.axis.delta_y2plot = gui.data.delta_P - 2*(gui.data.delta_S);
+        gui.axis.ylabelstr = 'Load oved Stiffness squared (1/GPa)';
+        gui.axis.ymax = round(max(gui.axis.y2plot.*1000)./10)./100;
+    elseif gui.variables.y_axis == 4
         gui.axis.y2plot = gui.results.Esample_red;
         gui.axis.delta_y2plot = 0;
         gui.axis.y2plot_2 = gui.results.Em_red;
         gui.axis.ylabelstr = 'Reduced Young''s modulus (sample) (GPa)';
         gui.axis.ymax = mean(gui.results.Esample_red) + ...
             0.3*mean(gui.results.Esample_red);
-        gui.axis.title_str = '';
-        
-    elseif gui.variables.y_axis == 4
+    elseif gui.variables.y_axis == 5
         gui.axis.y2plot = gui.results.Esample_red;
         gui.axis.delta_y2plot = 0;
         gui.axis.y2plot_2 = gui.results.Ef_red;
-        gui.axis.ylabelstr = 'Reduced Young''s modulus (film) vs. Young''s modulus(film + substrate) (GPa)';
+        gui.axis.ylabelstr = ['Reduced Young''s modulus (film) vs. ', ...
+            'Young''s modulus(film + substrate) (GPa)'];
         gui.axis.ymax = mean(gui.results.Esample_red) + ...
             0.3*mean(gui.results.Esample_red);
-        gui.axis.title_str = '';
-        
-    elseif gui.variables.y_axis == 5
+    elseif gui.variables.y_axis == 6
         gui.axis.y2plot = gui.results.H;
         gui.axis.delta_y2plot = 0;
         gui.axis.ylabelstr = 'Hardness (GPa)';
         gui.axis.ymax = mean(gui.results.H)+0.3*mean(gui.results.H);
         gui.axis.title_str = strcat('Mean Hardness = ', ...
             num2str(round(mean(gui.results.H.*1000)./10)./100), 'GPa');
-        
     end
     
 end
