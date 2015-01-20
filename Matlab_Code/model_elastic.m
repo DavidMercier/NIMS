d1d9f15332a58f2c2acf5b19215074dcc1ba822a
unknown <d.mercier@mapc103.mpie.de>
2015-01-20 15:25:17 +0100

    add elastic model

---
+++ Matlab_Code/model_elastic.m
@@ -0,0 +1,26 @@
+%% Copyright 2014 MERCIER David
+function model_elastic
+%% Function used for the calculation of the reduced Young's modulus
+
+gui = guidata(gcf);
+
+%% Young's modulus calculation - See in Pharr et al. (1992)
+
+gui.data.Eind     = gui.data.indenter_material_ym * 10^9; % Young's modulus of of indenter material (in Pa)
+gui.data.nuind    = gui.data.indenter_material_pr; % Poisson's coefficient of indenter material
+gui.data.Eind_red = (gui.data.Eind / (1-gui.data.nuind^2)); % Reduced Young's modulus of diamond. See in Fischer-Cripps "Nanoindentation 2nd Ed.".
+
+% Effective reduced Young's modulus (sample+indenter) in GPa
+gui.results.Eeff_red = ((pi^0.5)/(2*gui.data.beta .* gui.data.gamma)).* ...
+    10^6.*gui.data.S.*(1./sqrt(gui.results.Ac));
+
+% Reduced Young's modulus of the sample in GPa (no indenter's contribution)
+gui.results.Esample_red = ((1./gui.results.Eeff_red) - ...
+    (1/(1e-9*gui.data.Eind_red))).^-1;
+
+% Young's modulus of the sample in GPa
+gui.results.Esample = gui.results.Esample_red * (1-gui.data.nuf.^2);
+
+guidata(gcf, gui);
+
+end
