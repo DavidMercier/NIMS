7b92e7b07db828689adac2e54871dfec24ff60d7
David MERCIER <d.mercier@mpie.de>
2015-01-22 23:47:38 +0100

    changed conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -124,14 +124,15 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 #        #app.add_javascript("custom.js")                                         
 #        app.add_stylesheet('theme_overrides.css')                                
 #else:                                                                            
-    # Override default css to get a larger width for ReadTheDoc build            
-html_context = {                                                             
-	'css_files': [                                                           
-		'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
-		'https://media.readthedocs.org/css/readthedocs-doc-embed.css',       
-		'_static/theme_overrides.css',                                       
-	],                                                                       
-}
+    # Override default css to get a larger width for ReadTheDoc build    
+if	on_rtd:
+	html_context = {                                                             
+		'css_files': [                                                           
+			'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
+			'https://media.readthedocs.org/css/readthedocs-doc-embed.css',       
+			'_static/theme_overrides.css',                                       
+		],                                                                       
+	}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
