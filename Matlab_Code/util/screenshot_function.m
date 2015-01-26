2d2d5abe786ef57fd2cd12756f3f53989714964b
David MERCIER <d.mercier@mpie.de>
2015-01-26 06:47:20 +0100

    added screenshots function

---
+++ Matlab_Code/util/screenshot_function.m
@@ -0,0 +1,23 @@
+%% Copyright 2014 MERCIER David
+function SCREENSHOT_NUM = screenshot_function(...
+    SCREENSHOT, SCREENSHOT_DIR, SCREENSHOT_NUM, varargin)
+%% Function to save screenshots of the Matlab window
+% SCREENSHOT : Name of the screenshot
+% SCREENSHOT_DIR : Path to the directory to save the screenshot
+% SCREENSHOT_NUM : Number of the screenshot if sequential screenshots...
+
+if nargin == 0
+    SCREENSHOT = strcat(timestamp_make, 'screenshots');
+    SCREENSHOT_DIR = cd;
+    SCREENSHOT_NUM = 0;
+end
+
+if SCREENSHOT
+    im = screencapture(gcf);
+    imwrite(im, fullfile(SCREENSHOT_DIR,...
+        [SCREENSHOT, '_', num2str(SCREENSHOT_NUM), '.png']),'png');
+end
+
+SCREENSHOT_NUM = SCREENSHOT_NUM + 1;
+
+end
