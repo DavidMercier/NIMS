2ad5a9f7f760b8a4250df1857fe7da596268c603
David MERCIER <d.mercier@mpie.de>
2015-01-26 06:28:34 +0100

    added function to calculate epsilon of OliverPharr

---
+++ Matlab_Code/load_displacement/epsilon_OliverPharr.m
@@ -0,0 +1,21 @@
+%% Copyright 2014 MERCIER David
+function [epsilon, m] = epsilon_OliverPharr
+%% Function used to plot epsilon parameter in function of m coefficient
+% of unloading curves.
+% See Pharr G. M.  and Bolshakov A. (2002)- DOI: 10.1557/JMR.2002.0386
+
+m = 1:0.0001:2;
+m = m';
+
+epsilon = m.*(1 - ((2*gamma(m./(2*(m-1))) ./ ...
+    ((pi^0.5)*gamma(1./(2*(m-1))))).*(m-1)));
+
+plot(m, epsilon, 'b-', 'LineWidth', 2, 'MarkerSize', 10)
+
+xlabel('m', 'Color', [0,0,0], 'FontSize', 14);
+ylabel('\epsilon', 'Color', [0,0,0], 'FontSize', 14);
+set(gca, 'FontSize', 14);
+
+grid on;
+
+end
