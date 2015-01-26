d40850c713e21f3b394b07fed2c5c6281822f3f5
David MERCIER <d.mercier@mpie.de>
2015-01-26 07:41:48 +0100

    added picture for epsilon=f(m) + improved function

--- Matlab_Code/load_displacement/epsilon_OliverPharr.m
+++ Matlab_Code/load_displacement/epsilon_OliverPharr.m
@@ -4,18 +4,23 @@ function [epsilon, m] = epsilon_OliverPharr
 % of unloading curves.
 % See Pharr G. M.  and Bolshakov A. (2002)- DOI: 10.1557/JMR.2002.0386
 
+fig_epsilon = figure('Name', 'epsilon = f(m)',...
+    'NumberTitle', 'off');
+
 m = 1:0.0001:2;
 m = m';
 
 epsilon = m.*(1 - ((2*gamma(m./(2*(m-1))) ./ ...
     ((pi^0.5)*gamma(1./(2*(m-1))))).*(m-1)));
 
-plot(m, epsilon, 'b-', 'LineWidth', 2, 'MarkerSize', 10)
+plot(m, epsilon, 'b-', 'LineWidth', 2, 'MarkerSize', 10);
 
 xlabel('m', 'Color', [0,0,0], 'FontSize', 14);
 ylabel('\epsilon', 'Color', [0,0,0], 'FontSize', 14);
 set(gca, 'FontSize', 14);
 
 grid on;
+
+%save_figure(pwd, gca, '_epsilonOliverPharr');
 
 end
