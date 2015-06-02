71d2d20817f2fbdee3f80b68c980dc043ca67ef6
david mercier <d.mercier@mpie.de>
2015-06-02 11:45:31 +0200

    updated functions for elastic bilayer models based on Doerner and Nix model

--- Matlab_Code/elastic_models/model_doerner_nix.m
+++ Matlab_Code/elastic_models/model_doerner_nix.m
@@ -1,7 +1,7 @@
 %% Copyright 2014 MERCIER David
 function model_doerner_nix(OPTIONS)
 %% Function used to calculate Young's modulus in bilayer system with
-% the model of Doerner & Nix (1986) modified by King (1987)
+% the model of Doerner & Nix (1986)
 gui = guidata(gcf);
 
 x = gui.results.t_corr./gui.results.hc;
@@ -12,7 +12,7 @@ x = checkValues(x);
 bilayer_model = @(A, x) ...
     (1e-9*(((1./(1e9*A(1)))*(1-exp(-A(2)*x))) + ...
     ((1./gui.data.Es_red)*(exp(-A(2)*x)))).^-1);
-	
+
 % Minimum and maximum boundaries for the constant alpha
 min_alpha = gui.config.numerics.alpha_min_DoernerNix;
 max_alpha = gui.config.numerics.alpha_max_DoernerNix;
@@ -42,7 +42,8 @@ gui.results.Ef_sol_fit = non_reduced_YM(gui.results.Ef_red_sol_fit(1), ...
 gui.results.Ef_sol_fit(2) = gui.results.Ef_red_sol_fit(2);
 
 gui.results.Em_red = ...
-    (1e-9*(((1./(1e9*gui.results.Ef_red_sol_fit(1)))*(1-exp(-gui.results.Ef_red_sol_fit(2)*x))) + ...
+    (1e-9*(((1./(1e9*gui.results.Ef_red_sol_fit(1)))*...
+    (1-exp(-gui.results.Ef_red_sol_fit(2)*x))) + ...
     ((1./gui.data.Es_red)*(exp(-gui.results.Ef_red_sol_fit(2)*x)))).^-1);
 
 gui.results.Ef_red = 1e-9*(((1./(1e9.*gui.results.Esample_red)) - ...
--- Matlab_Code/elastic_models/model_doerner_nix_saha.m
+++ Matlab_Code/elastic_models/model_doerner_nix_saha.m
@@ -1,15 +1,28 @@
 %% Copyright 2014 MERCIER David
 function model_doerner_nix_saha(OPTIONS)
 %% Function used to calculate Young's modulus in bilayer system with
-% the model of Doerner & Nix (1986) modified by King (1987)
+% the model of Doerner & Nix (1986) modified by Saha (2002)
 gui = guidata(gcf);
-
 
 if get(gui.handles.cb_corr_thickness_GUI, 'Value') == 1
     set(gui.handles.cb_corr_thickness_GUI, 'Value', 0);
     commandwindow;
     warning(['The correction of the thickness can''t be used ', ...
         'with Saha''s model']);
+end
+
+% !!! Only with Oliver and Pharr model otherwise hc > t for some values
+% x becomes negative... giving wrong values.
+
+if get(gui.handles.value_modeldisp_GUI, 'Value') ~= 2
+    set(gui.handles.value_modeldisp_GUI, 'Value', 2); % Oliver and Pharr
+    gui.variables.val1 = get(gui.handles.value_modeldisp_GUI, 'Value');
+    guidata(gcf, gui);
+    model_function_area;
+    gui = guidata(gcf); guidata(gcf, gui);
+    commandwindow;
+    warning(['Saha''s model can be only used with Oliver and Pharr''s ', ...
+        'model !']);
 end
 
 x = (gui.data.t - gui.results.hc)./gui.results.ac;
@@ -20,7 +33,7 @@ x = checkValues(x);
 bilayer_model = @(A, x) ...
     (1e-9*(((1./(1e9*A(1)))*(1-exp(-A(2)*x))) + ...
     ((1./gui.data.Es_red)*(exp(-A(2)*x)))).^-1);
-	
+
 % Minimum and maximum boundaries for the constant alpha
 min_alpha = gui.config.numerics.alpha_min_DoernerNix;
 max_alpha = gui.config.numerics.alpha_max_DoernerNix;
@@ -50,11 +63,13 @@ gui.results.Ef_sol_fit = non_reduced_YM(gui.results.Ef_red_sol_fit(1), ...
 gui.results.Ef_sol_fit(2) = gui.results.Ef_red_sol_fit(2);
 
 gui.results.Em_red = ...
-    (1e-9*(((1./(1e9*gui.results.Ef_red_sol_fit(1)))*(1-exp(-gui.results.Ef_red_sol_fit(2)*x))) + ...
+    (1e-9*(((1./(1e9*gui.results.Ef_red_sol_fit(1)))*...
+    (1-exp(-gui.results.Ef_red_sol_fit(2)*x))) + ...
     ((1./gui.data.Es_red)*(exp(-gui.results.Ef_red_sol_fit(2)*x)))).^-1);
 
 gui.results.Ef_red = 1e-9*(((1./(1e9.*gui.results.Esample_red)) - ...
-    ((1./gui.data.Es_red)*(exp(-gui.results.Ef_red_sol_fit(2).*x))))./(1-exp(-gui.results.Ef_red_sol_fit(2).*x))).^-1;
+    ((1./gui.data.Es_red)*(exp(-gui.results.Ef_red_sol_fit(2).*x))))./...
+    (1-exp(-gui.results.Ef_red_sol_fit(2).*x))).^-1;
 
 guidata(gcf, gui);
 
