5bbfd721d3fc2f13f8fbeb5ed404bf23885c1e53
David MERCIER <d.mercier@mpie.de>
2015-06-07 20:04:31 +0200

    some corrections of small bugs

--- Matlab_Code/gui/emptyVariables.m
+++ Matlab_Code/gui/emptyVariables.m
@@ -2,22 +2,26 @@
 function emptyVariables
 %% Function used to set empty variables
 
-% Preallocation
-gui.results.Ef     = NaN(length(gui.data.h), 1);
-gui.results.Ef_red = NaN(length(gui.data.h), 1);
-gui.results.Em_red = NaN(length(gui.data.h), 1);
-gui.results.Hf = NaN(length(gui.data.h), 1);
+g = guidata(gcf);
 
-for ii = 1:1:length(gui.data.h)
-    gui.results.Ef(ii)      = 0;
-    gui.results.Ef          = gui.results.Ef.';
-    gui.results.Ef_red(ii)  = 0;
-    gui.results.Ef_red      = gui.results.Ef_red.';
-    gui.results.Ef_sol_fit  = 0;
-    gui.results.Em_red(ii)  = 0;
-    gui.results.Em_red      = gui.results.Em_red.';
-    gui.results.Hf(ii) = 0;
-    gui.results.Hf     = gui.results.Ef.';
+% Preallocation
+g.results.Ef     = NaN(length(g.data.h), 1);
+g.results.Ef_red = NaN(length(g.data.h), 1);
+g.results.Em_red = NaN(length(g.data.h), 1);
+g.results.Hf = NaN(length(g.data.h), 1);
+
+for ii = 1:1:length(g.data.h)
+    g.results.Ef(ii)      = 0;
+    g.results.Ef          = g.results.Ef.';
+    g.results.Ef_red(ii)  = 0;
+    g.results.Ef_red      = g.results.Ef_red.';
+    g.results.Ef_sol_fit  = 0;
+    g.results.Em_red(ii)  = 0;
+    g.results.Em_red      = g.results.Em_red.';
+    g.results.Hf(ii) = 0;
+    g.results.Hf     = g.results.Ef.';
 end
+
+guidqtq(gcf, g);
 
 end
