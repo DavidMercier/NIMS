024588203e1cc54614c118a1fdc6407aded49b26
David Mercier <david9684@gmail.com>
2016-08-06 23:40:27 +0200

    added missing file for optimization process

---
+++ matlab_code/minimizationProcess.m
@@ -0,0 +1,16 @@
+%% Copyright 2014 MERCIER David
+function [X,RESNORM,RESIDUAL,EXITFLAG,OUTPUT,LAMBDA,JACOBIAN] = ...
+    minimizationProcess(FUN,X0,XDATA,YDATA,LB,UB,OPTIONS)
+%% Function used to process the minimization
+
+resultToolbox = isToolboxAvailable('Optimization Toolbox');
+
+if resultToolbox
+    [X,RESNORM,RESIDUAL,EXITFLAG,OUTPUT,LAMBDA,JACOBIAN] = lsqcurvefit(...
+        FUN,X0,XDATA,YDATA,LB,UB,OPTIONS);
+else
+    errordlg('No Optimization toolbox availble !');
+end
+
+end
+
